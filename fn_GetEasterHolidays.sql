959b708ba1dd70fcc507fbe5984bb5208a0b58ff
James-Poland <47355864+James-Poland@users.noreply.github.com>
2026-06-29 10:53:33 +0100

    Update datefrom parts instead of string

--- fn_GetEasterHolidays.sql
+++ fn_GetEasterHolidays.sql
@@ -1,4 +1,4 @@
-﻿Create or ALTER FUNCTION [dbo].[GetEasterHolidays](@year INT) 
+CREATE OR ALTER FUNCTION [dbo].[GetEasterHolidays](@year INT) 
 RETURNS TABLE
 WITH SCHEMABINDING
 AS 
@@ -6,17 +6,17 @@ RETURN
 (
   WITH x AS 
   (
-    SELECT [Date] = CONVERT(DATE, RTRIM(@year) + '0' + RTRIM([Month]) 
-        + RIGHT('0' + RTRIM([Day]),2))
+    
+    SELECT [Date] = DATEFROMPARTS(@year, [Month], [Day])
       FROM (SELECT [Month], [Day] = DaysToSunday + 28 - (31 * ([Month] / 4))
       FROM (SELECT [Month] = 3 + (DaysToSunday + 40) / 44, DaysToSunday
-      FROM (SELECT DaysToSunday = paschal - ((@year + @year / 4 + paschal - 13) % 7)
+          FROM (SELECT DaysToSunday = paschal - ((@year + @year / 4 + paschal - 13) % 7)
       FROM (SELECT paschal = epact - (epact / 28)
       FROM (SELECT epact = (24 + 19 * (@year % 19)) % 30) 
         AS epact) AS paschal) AS dts) AS m) AS d
   )
-  SELECT [Date], HolidayName = 'Easter Sunday' FROM x
-    UNION ALL SELECT DATEADD(DAY,-2,[Date]), 'Good Friday'   FROM x
-    UNION ALL SELECT DATEADD(DAY, 1,[Date]), 'Easter Monday' FROM x
-);
 
+  SELECT [Date], CAST('Easter Sunday' AS VARCHAR(30)) AS HolidayName FROM x
+    UNION ALL SELECT DATEADD(DAY, -2, [Date]), 'Good Friday'   FROM x
+    UNION ALL SELECT DATEADD(DAY,  1, [Date]), 'Easter Monday' FROM x
+);
